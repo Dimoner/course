1592892b8a8659aa571aa44255c95e8c4f0e693c
daHil <ssocmain@gmail.com>
2024-02-26 15:08:47 +0500

    added user repo

---
+++ IdentityServiceApi/Dal/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Dal.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> Get(Guid id);
+        Task<bool> Create(T item);
+        Task<T> Update(T item);
+        Task<bool> Delete(Guid id);
+    }
+}
---
+++ IdentityServiceApi/Dal/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using Dal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly IdentityServiceContext context;
+        public UserRepository(IdentityServiceContext context) 
+        { 
+            this.context = context;
+        }
+        public async Task<bool> Create(User user)
+        {
+            var addedUser = await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            return addedUser.State == EntityState.Added;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if (user == null)
+                return false;
+            var removedUser = context.Users.Remove(user);
+            await context.SaveChangesAsync();
+
+            return removedUser.State == EntityState.Deleted;
+        }
+
+        public async Task<User?> Get(Guid id)
+        {
+            return await context.Users.FindAsync(id);
+        }
+
+        public async Task<User> Update(User user)
+        {
+            var updatedUser = context.Users.Update(user);
+            await context.SaveChangesAsync();
+            
+            return updatedUser.Entity;
+        }
+    }
+}
