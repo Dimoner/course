b0ca6660e88b2ac5ca76dc4a608098ad872c5d6c
daHil <ssocmain@gmail.com>
2024-02-26 15:07:41 +0500

    created dbcontext for identity service

---
+++ IdentityServiceApi/Dal/IdentityServiceContext.cs
@@ -0,0 +1,15 @@
+﻿using Dal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal
+{
+    public class IdentityServiceContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public IdentityServiceContext(DbContextOptions<IdentityServiceContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   
+        }
+    }
+}
