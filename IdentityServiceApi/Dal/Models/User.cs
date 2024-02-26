7032b898c25a0a799dc7de15e99e397ff19d85b2
daHil <ssocmain@gmail.com>
2024-02-26 15:06:59 +0500

    added user model

---
+++ IdentityServiceApi/Dal/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Core.Dal.Base;
+
+namespace Dal.Models
+{
+    public record User: BaseEntity
+    {
+        public string FirstName { get; init; } = null!;
+        public string SecondName { get; init; } = null!;
+        public string Email { get; init; } = null!;
+        public int Age { get; init; }
+        public DateTime RegistrationDate { get; init; }
+        public string Password { get; init; } = null!;
+    }
+}
