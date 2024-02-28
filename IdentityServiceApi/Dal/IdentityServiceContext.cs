8a2208e89c5320a710abdf983dce8ee5a9ba83bf
daHil <ssocmain@gmail.com>
2024-02-28 15:27:39 +0500

    added friendship dbset

--- IdentityServiceApi/Dal/IdentityServiceContext.cs
+++ IdentityServiceApi/Dal/IdentityServiceContext.cs
@@ -7,6 +7,7 @@ namespace Dal
     {
         public DbSet<UserDal> Users { get; set; }
         public DbSet<UserProfileDal> UserProfiles { get; set; }
+        public DbSet<FriendshipDal> Friendships { get; set; }
         public IdentityServiceContext(DbContextOptions<IdentityServiceContext> options)
             : base(options)
         {
