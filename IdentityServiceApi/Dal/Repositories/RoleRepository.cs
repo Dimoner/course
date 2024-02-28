b2734a500d0ce2a4dc45d7428189c2d67fd40e18
daHil <ssocmain@gmail.com>
2024-02-29 01:27:00 +0500

    added role repo

---
+++ IdentityServiceApi/Dal/Repositories/RoleRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal.Repositories
+{
+    public class RoleRepository: IRepository<RoleDal>
+    {
+        private readonly IdentityServiceContext context;
+
+        public RoleRepository(IdentityServiceContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> Create(RoleDal role)
+        {
+            var addedRole = await context.Roles.AddAsync(role);
+            await context.SaveChangesAsync();
+
+            return addedRole.State == EntityState.Added;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var role = await context.Roles.FindAsync(id);
+            if (role == null)
+                return false;
+
+            var removedRole = context.Roles.Remove(role);
+            await context.SaveChangesAsync();
+
+            return removedRole.State == EntityState.Deleted;
+        }
+
+        public async Task<RoleDal?> Get(Guid userId)
+        {
+            return await context.Roles.FindAsync(userId);
+        }
+
+        public async Task<RoleDal> Update(RoleDal role)
+        {
+            var updatedRole = context.Roles.Update(role);
+            await context.SaveChangesAsync();
+
+            return updatedRole.Entity;
+        }
+    }
+}
