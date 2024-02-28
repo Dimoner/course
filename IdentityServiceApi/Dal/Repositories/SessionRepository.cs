df5d443cc8c5222ca6b41c4c9a80f1c1f34dda24
daHil <ssocmain@gmail.com>
2024-02-29 00:37:31 +0500

    added sessions repo

---
+++ IdentityServiceApi/Dal/Repositories/SessionRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal.Repositories
+{
+    public class SessionRepository: IRepository<SessionDal>
+    {
+        private readonly IdentityServiceContext context;
+
+        public SessionRepository(IdentityServiceContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> Create(SessionDal session)
+        {
+            var addedSession = await context.Sessions.AddAsync(session);
+            await context.SaveChangesAsync();
+
+            return addedSession.State == EntityState.Added;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var session = await context.Sessions.FindAsync(id);
+            if (session == null)
+                return false;
+
+            var removedSession = context.Sessions.Remove(session);
+            await context.SaveChangesAsync();
+
+            return removedSession.State == EntityState.Deleted;
+        }
+
+        public async Task<SessionDal?> Get(Guid id)
+        {
+            return await context.Sessions.FindAsync(id);
+        }
+
+        public async Task<SessionDal> Update(SessionDal session)
+        {
+            var updatedSession = context.Sessions.Update(session);
+            await context.SaveChangesAsync();
+
+            return updatedSession.Entity;
+        }
+    }
+}
