3c4f773defc71ca827649ae1e72f54faf8459b1c
daHil <ssocmain@gmail.com>
2024-02-29 01:32:53 +0500

    added rights repo

---
+++ IdentityServiceApi/Dal/Repositories/RightRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal.Repositories
+{
+    public class RightRepository: IRepository<RightDal>
+    {
+        private readonly IdentityServiceContext context;
+
+        public RightRepository(IdentityServiceContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> Create(RightDal right)
+        {
+            var addedRight = await context.Rights.AddAsync(right);
+            await context.SaveChangesAsync();
+
+            return addedRight.State == EntityState.Added;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var right = await context.Rights.FindAsync(id);
+            if (right == null)
+                return false;
+
+            var removedRight = context.Rights.Remove(right);
+            await context.SaveChangesAsync();
+
+            return removedRight.State == EntityState.Deleted;
+        }
+
+        public async Task<RightDal?> Get(Guid id)
+        {
+            return await context.Rights.FindAsync(id);
+        }
+
+        public async Task<RightDal> Update(RightDal right)
+        {
+            var updatedRight = context.Rights.Update(right);
+            await context.SaveChangesAsync();
+
+            return updatedRight.Entity;
+        }
+    }
+}
