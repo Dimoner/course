799fed027e7e3c04116ed1bfdde9f90f32ada5c5
daHil <ssocmain@gmail.com>
2024-02-29 01:32:41 +0500

    added rights dbset

--- IdentityServiceApi/Dal/IdentityServiceContext.cs
+++ IdentityServiceApi/Dal/IdentityServiceContext.cs
@@ -10,6 +10,7 @@ namespace Dal
         public DbSet<FriendshipDal> Friendships { get; set; }
         public DbSet<RoleDal> Roles { get; set; }
         public DbSet<SessionDal> Sessions { get; set; }
+        public DbSet<RightDal> Rights { get; set; }
         public IdentityServiceContext(DbContextOptions<IdentityServiceContext> options)
             : base(options)
         {
