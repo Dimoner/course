87f14545c5173822b52e29797a0fac79066e3f6a
daHil <ssocmain@gmail.com>
2024-02-29 17:21:15 +0500

    added user profile manager

---
+++ IdentityServiceApi/Logic/UserProfiles/Managers/IUserProfileLogicManager.cs
@@ -0,0 +1,13 @@
+﻿using Logic.UserProfiles.Models;
+
+namespace Logic.UserProfiles.Managers
+{
+    public interface IUserProfileLogicManager
+    {
+        Task<UserProfileLogic> GetUserProfileAsync(Guid userId);
+        Task<IEnumerable<UserProfileLogic>> GetAllUsersProfilesAsync();
+        Task<Guid> CreateUserProfileAsync(UserProfileLogic user);
+        Task<bool> DeleteUserProfileAsync(Guid userId);
+        Task<UserProfileLogic> UpdateUserProfileAsync(UserProfileLogic user);
+    }
+}
---
+++ IdentityServiceApi/Logic/UserProfiles/Managers/UserProfileLogicManager.cs
@@ -0,0 +1,83 @@
+﻿using Dal.UserProfiles;
+using Logic.UserProfiles.Models;
+
+namespace Logic.UserProfiles.Managers
+{
+    public class UserProfileLogicManager : IUserProfileLogicManager
+    {
+        private readonly IUserProfileRepository profileRepository;
+
+        public UserProfileLogicManager(IUserProfileRepository profileRepository)
+        {
+            this.profileRepository = profileRepository;
+        }
+
+        public async Task<Guid> CreateUserProfileAsync(UserProfileLogic profile)
+        {
+            return await profileRepository.CreateAsync(new UserProfileDal
+            {
+                AvatarUrl = profile.AvatarUrl,
+                BirthDate = profile.BirthDate,
+                Id = profile.Id,
+                Status = profile.Status,
+                UserId = profile.Id
+            });
+        }
+
+        public async Task<bool> DeleteUserProfileAsync(Guid profileId)
+        {
+            return await profileRepository.DeleteAsync(profileId);
+        }
+
+        public async Task<IEnumerable<UserProfileLogic>> GetAllUsersProfilesAsync()
+        {
+            var profiles = await profileRepository.GetAllAsync();
+
+            return profiles.Select(profile => new UserProfileLogic
+            {
+                AvatarUrl = profile.AvatarUrl,
+                BirthDate = profile.BirthDate,
+                Id = profile.Id,
+                UserId = profile.UserId,
+                Status = profile.Status
+            });
+        }
+
+        public async Task<UserProfileLogic> GetUserProfileAsync(Guid profileId)
+        {
+            var profile = await profileRepository.GetAsync(profileId);
+
+            return profile == null
+                ? new UserProfileLogic()
+                : new UserProfileLogic
+                {
+                    AvatarUrl = profile.AvatarUrl,
+                    BirthDate = profile.BirthDate,
+                    Id = profile.Id,
+                    Status = profile.Status,
+                    UserId = profileId
+                };
+        }
+
+        public async Task<UserProfileLogic> UpdateUserProfileAsync(UserProfileLogic profile)
+        {
+            var updatedProfile = await profileRepository.UpdateAsync(new UserProfileDal
+            {
+                UserId = profile.Id,
+                AvatarUrl = profile.AvatarUrl,
+                BirthDate = profile.BirthDate,
+                Id = profile.Id,
+                Status = profile.Status,
+            });
+
+            return new UserProfileLogic
+            {
+                AvatarUrl = updatedProfile.AvatarUrl,
+                BirthDate = updatedProfile.BirthDate,
+                Id = updatedProfile.Id,
+                Status = updatedProfile.Status,
+                UserId = updatedProfile.UserId
+            };
+        }
+    }
+}
