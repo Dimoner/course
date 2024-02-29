04783e03290ebf14e5e72dfc54bb6245b5e9fb59
daHil <ssocmain@gmail.com>
2024-02-29 17:20:22 +0500

    added user profile logic model

---
+++ IdentityServiceApi/Logic/UserProfiles/Models/UserProfileLogic.cs
@@ -0,0 +1,11 @@
+﻿namespace Logic.UserProfiles.Models
+{
+    public record UserProfileLogic
+    {
+        public Guid Id { get; init; }
+        public Guid UserId { get; init; }
+        public string? Status { get; init; }
+        public DateTime? BirthDate { get; init; }
+        public string AvatarUrl { get; init; } = null!;
+    }
+}
