ce5cb8ec3f5e8d2870b43c8cca12b7acbccd6985
daHil <ssocmain@gmail.com>
2024-02-29 16:20:14 +0500

    added user model

---
+++ IdentityServiceApi/Logic/Users/Models/UserLogic.cs
@@ -0,0 +1,14 @@
+﻿namespace Logic.Users.Models
+{
+    public record UserLogic
+    {
+        public Guid Id { get; init; }
+        public string FirstName { get; init; } = null!;
+        public string SecondName { get; init; } = null!;
+        public string Email { get; init; } = null!;
+        public int Age { get; init; }
+        public DateTime RegistrationDate { get; init; }
+        public string Password { get; init; } = null!;
+        public Guid RoleId { get; init; }
+    }
+}
