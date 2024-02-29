b1ee2c3411fb657c8b242374ea609c96c7a7bd97
daHil <ssocmain@gmail.com>
2024-02-29 16:21:06 +0500

    added user manager interface

---
+++ IdentityServiceApi/Logic/Users/Managers/IUserLogicManager.cs
@@ -0,0 +1,13 @@
+﻿using Logic.Users.Models;
+
+namespace Logic.Users.Managers
+{
+    public interface IUserLogicManager
+    {
+        Task<UserLogic> GetUserAsync(Guid userId);
+        Task<IEnumerable<UserLogic>> GetAllUsersAsync();
+        Task<Guid> CreateUserAsync(UserLogic user);
+        Task<bool> DeleteUserAsync(Guid userId);
+        Task<UserLogic> UpdateUserAsync(UserLogic user);
+    }
+}
