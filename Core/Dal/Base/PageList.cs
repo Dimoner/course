8277dd1371252552f143d758854557efe45239d2
daHil <ssocmain@gmail.com>
2024-03-01 01:49:55 +0500

    added PageList

---
+++ Core/Dal/Base/PageList.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Dal.Base
+{
+    public class PageList<T> : List<T>
+    {
+        public int CurrentPage { get; }
+        public int TotalPages { get; }
+        public int PageSize { get; }
+        public long TotalCount { get; }
+
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
+        public PageList(List<T> items, long totalCount, int currentPage, int pageSize)
+        {
+            TotalCount = totalCount;
+            PageSize = pageSize;
+            CurrentPage = currentPage;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            AddRange(items);
+        }
+    }
+}
