4edd3497c80b0db111195abd9f2c8f8252d6ea68
daHil <ssocmain@gmail.com>
2024-02-29 17:29:34 +0500

    added refs to di

--- IdentityServiceApi/Api/Program.cs
+++ IdentityServiceApi/Api/Program.cs
@@ -1,3 +1,14 @@
+using Dal.FriendRequests;
+using Dal.Friendships;
+using Dal.RefreshTokens;
+using Dal.Rights;
+using Dal.Roles;
+using Dal.Sessions;
+using Dal.UserProfiles;
+using Dal.Users;
+using Logic.UserProfiles.Managers;
+using Logic.Users.Managers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +17,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// adding dal refs
+builder.Services.AddSingleton<IFriendRequestRepository, FriendRequestRepository>();
+builder.Services.AddSingleton<IFriendshipRepository, FriendshipRepository>();
+builder.Services.AddSingleton<IRefreshTokenRepository, RefreshTokenRepository>();
+builder.Services.AddSingleton<IRightRepository, RightRepository>();
+builder.Services.AddSingleton<IRoleRepository, RoleRepository>();
+builder.Services.AddSingleton<ISessionRepository, SessionRepository>();
+builder.Services.AddSingleton<IUserProfileRepository, UserProfileRepository>();
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+
+// adding logic refs
+builder.Services.AddSingleton<IUserProfileLogicManager, UserProfileLogicManager>();
+builder.Services.AddSingleton<IUserLogicManager, UserLogicManager>();
 
 var app = builder.Build();
 
