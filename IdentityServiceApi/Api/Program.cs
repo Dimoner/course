f2908cb9e0cb9077b5060051b7dece00e96d948c
daHil <ssocmain@gmail.com>
2024-03-01 02:14:14 +0500

    added controllers options

--- IdentityServiceApi/Api/Program.cs
+++ IdentityServiceApi/Api/Program.cs
@@ -8,12 +8,19 @@ using Dal.UserProfiles;
 using Dal.Users;
 using Logic.UserProfiles.Managers;
 using Logic.Users.Managers;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+    options.ReturnHttpNotAcceptable = true;
+    options.RespectBrowserAcceptHeader = true;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
