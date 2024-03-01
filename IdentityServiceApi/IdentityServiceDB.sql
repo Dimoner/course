1263f529bdfa02155de8050b446c548b097eed68
daHil <ssocmain@gmail.com>
2024-03-01 21:29:23 +0500

    refactored

--- IdentityServiceApi/IdentityServiceDB.sql
+++ IdentityServiceApi/IdentityServiceDB.sql
@@ -21,10 +21,10 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: FriendRequest; Type: TABLE; Schema: public; Owner: postgres
+-- Name: FriendRequests; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."FriendRequest" (
+CREATE TABLE public."FriendRequests" (
     "Id" uuid NOT NULL,
     "SenderUserId" uuid NOT NULL,
     "ReceiverUserId" uuid NOT NULL,
@@ -32,26 +32,26 @@ CREATE TABLE public."FriendRequest" (
 );
 
 
-ALTER TABLE public."FriendRequest" OWNER TO postgres;
+ALTER TABLE public."FriendRequests" OWNER TO postgres;
 
 --
--- Name: Friendship; Type: TABLE; Schema: public; Owner: postgres
+-- Name: Friendships; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."Friendship" (
+CREATE TABLE public."Friendships" (
     "Id" uuid NOT NULL,
     "UserId1" uuid NOT NULL,
     "UserId2" uuid NOT NULL
 );
 
 
-ALTER TABLE public."Friendship" OWNER TO postgres;
+ALTER TABLE public."Friendships" OWNER TO postgres;
 
 --
--- Name: RefreshToken; Type: TABLE; Schema: public; Owner: postgres
+-- Name: RefreshTokens; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."RefreshToken" (
+CREATE TABLE public."RefreshTokens" (
     "Id" uuid NOT NULL,
     "UserId" uuid NOT NULL,
     "Token" text NOT NULL,
@@ -59,31 +59,19 @@ CREATE TABLE public."RefreshToken" (
 );
 
 
-ALTER TABLE public."RefreshToken" OWNER TO postgres;
+ALTER TABLE public."RefreshTokens" OWNER TO postgres;
 
 --
--- Name: Right; Type: TABLE; Schema: public; Owner: postgres
+-- Name: Rights; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."Right" (
+CREATE TABLE public."Rights" (
     "Id" uuid NOT NULL,
     "Name" character varying(50)
 );
 
 
-ALTER TABLE public."Right" OWNER TO postgres;
-
---
--- Name: Role; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public."Role" (
-    "Id" uuid NOT NULL,
-    "Name" character varying(50)
-);
-
-
-ALTER TABLE public."Role" OWNER TO postgres;
+ALTER TABLE public."Rights" OWNER TO postgres;
 
 --
 -- Name: RoleRights; Type: TABLE; Schema: public; Owner: postgres
@@ -98,10 +86,22 @@ CREATE TABLE public."RoleRights" (
 ALTER TABLE public."RoleRights" OWNER TO postgres;
 
 --
--- Name: Session; Type: TABLE; Schema: public; Owner: postgres
+-- Name: Roles; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."Session" (
+CREATE TABLE public."Roles" (
+    "Id" uuid NOT NULL,
+    "Name" character varying(50)
+);
+
+
+ALTER TABLE public."Roles" OWNER TO postgres;
+
+--
+-- Name: Sessions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Sessions" (
     "Id" uuid NOT NULL,
     "UserId" uuid NOT NULL,
     "Token" text NOT NULL,
@@ -110,13 +110,28 @@ CREATE TABLE public."Session" (
 );
 
 
-ALTER TABLE public."Session" OWNER TO postgres;
+ALTER TABLE public."Sessions" OWNER TO postgres;
 
 --
--- Name: User; Type: TABLE; Schema: public; Owner: postgres
+-- Name: UserProfiles; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."User" (
+CREATE TABLE public."UserProfiles" (
+    "Id" uuid NOT NULL,
+    "Status" character varying(50),
+    "AvatarUrl" character varying(100) NOT NULL,
+    "RegistrationDate" date,
+    "UserId" uuid NOT NULL
+);
+
+
+ALTER TABLE public."UserProfiles" OWNER TO postgres;
+
+--
+-- Name: Users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Users" (
     "Id" uuid NOT NULL,
     "FirstName" character varying(50) NOT NULL,
     "LastName" character varying(50) NOT NULL,
@@ -128,60 +143,37 @@ CREATE TABLE public."User" (
 );
 
 
-ALTER TABLE public."User" OWNER TO postgres;
+ALTER TABLE public."Users" OWNER TO postgres;
 
 --
--- Name: UserProfile; Type: TABLE; Schema: public; Owner: postgres
+-- Data for Name: FriendRequests; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."UserProfile" (
-    "Id" uuid NOT NULL,
-    "Status" character varying(50),
-    "AvatarUrl" character varying(100) NOT NULL,
-    "RegistrationDate" date,
-    "UserId" uuid NOT NULL
-);
-
-
-ALTER TABLE public."UserProfile" OWNER TO postgres;
-
---
--- Data for Name: FriendRequest; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public."FriendRequest" ("Id", "SenderUserId", "ReceiverUserId", "Status") FROM stdin;
+COPY public."FriendRequests" ("Id", "SenderUserId", "ReceiverUserId", "Status") FROM stdin;
 \.
 
 
 --
--- Data for Name: Friendship; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: Friendships; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Friendship" ("Id", "UserId1", "UserId2") FROM stdin;
+COPY public."Friendships" ("Id", "UserId1", "UserId2") FROM stdin;
 \.
 
 
 --
--- Data for Name: RefreshToken; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: RefreshTokens; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."RefreshToken" ("Id", "UserId", "Token", "ExpiresAt") FROM stdin;
+COPY public."RefreshTokens" ("Id", "UserId", "Token", "ExpiresAt") FROM stdin;
 \.
 
 
 --
--- Data for Name: Right; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: Rights; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Right" ("Id", "Name") FROM stdin;
-\.
-
-
---
--- Data for Name: Role; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public."Role" ("Id", "Name") FROM stdin;
+COPY public."Rights" ("Id", "Name") FROM stdin;
 \.
 
 
@@ -194,50 +186,58 @@ COPY public."RoleRights" ("RoleId", "RightId") FROM stdin;
 
 
 --
--- Data for Name: Session; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: Roles; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Session" ("Id", "UserId", "Token", "CreatedAt", "ExpiresAt") FROM stdin;
+COPY public."Roles" ("Id", "Name") FROM stdin;
 \.
 
 
 --
--- Data for Name: User; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: Sessions; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."User" ("Id", "FirstName", "LastName", "Email", "Age", "RegistrationDate", "Password", "RoleId") FROM stdin;
+COPY public."Sessions" ("Id", "UserId", "Token", "CreatedAt", "ExpiresAt") FROM stdin;
 \.
 
 
 --
--- Data for Name: UserProfile; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: UserProfiles; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."UserProfile" ("Id", "Status", "AvatarUrl", "RegistrationDate", "UserId") FROM stdin;
+COPY public."UserProfiles" ("Id", "Status", "AvatarUrl", "RegistrationDate", "UserId") FROM stdin;
 \.
 
 
 --
--- Name: Friendship Friendship_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Data for Name: Users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Friendship"
+COPY public."Users" ("Id", "FirstName", "LastName", "Email", "Age", "RegistrationDate", "Password", "RoleId") FROM stdin;
+\.
+
+
+--
+-- Name: Friendships Friendship_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Friendships"
     ADD CONSTRAINT "Friendship_pkey" PRIMARY KEY ("Id", "UserId1", "UserId2");
 
 
 --
--- Name: RefreshToken RefreshToken_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: RefreshTokens RefreshToken_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."RefreshToken"
+ALTER TABLE ONLY public."RefreshTokens"
     ADD CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("Id");
 
 
 --
--- Name: Right Right_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: Rights Right_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Right"
+ALTER TABLE ONLY public."Rights"
     ADD CONSTRAINT "Right_pkey" PRIMARY KEY ("Id");
 
 
@@ -250,83 +250,83 @@ ALTER TABLE ONLY public."RoleRights"
 
 
 --
--- Name: Role Role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: Roles Role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Role"
+ALTER TABLE ONLY public."Roles"
     ADD CONSTRAINT "Role_pkey" PRIMARY KEY ("Id");
 
 
 --
--- Name: FriendRequest friendrequest_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: FriendRequests friendrequest_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."FriendRequest"
+ALTER TABLE ONLY public."FriendRequests"
     ADD CONSTRAINT friendrequest_pkey PRIMARY KEY ("Id");
 
 
 --
--- Name: Session session_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: Sessions session_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Session"
+ALTER TABLE ONLY public."Sessions"
     ADD CONSTRAINT session_pkey PRIMARY KEY ("Id");
 
 
 --
--- Name: UserProfile userprofiles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: UserProfiles userprofiles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."UserProfile"
+ALTER TABLE ONLY public."UserProfiles"
     ADD CONSTRAINT userprofiles_pkey PRIMARY KEY ("Id");
 
 
 --
--- Name: UserProfile userprofiles_user_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: UserProfiles userprofiles_user_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."UserProfile"
+ALTER TABLE ONLY public."UserProfiles"
     ADD CONSTRAINT userprofiles_user_id_key UNIQUE ("UserId");
 
 
 --
--- Name: User users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: Users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."User"
+ALTER TABLE ONLY public."Users"
     ADD CONSTRAINT users_email_key UNIQUE ("Email");
 
 
 --
--- Name: User users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: Users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."User"
+ALTER TABLE ONLY public."Users"
     ADD CONSTRAINT users_pkey PRIMARY KEY ("Id");
 
 
 --
--- Name: Friendship Friendship_UserId1_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: Friendships Friendship_UserId1_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Friendship"
-    ADD CONSTRAINT "Friendship_UserId1_fkey" FOREIGN KEY ("UserId1") REFERENCES public."User"("Id");
-
-
---
--- Name: Friendship Friendship_UserId2_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."Friendship"
-    ADD CONSTRAINT "Friendship_UserId2_fkey" FOREIGN KEY ("UserId2") REFERENCES public."User"("Id");
+ALTER TABLE ONLY public."Friendships"
+    ADD CONSTRAINT "Friendship_UserId1_fkey" FOREIGN KEY ("UserId1") REFERENCES public."Users"("Id");
 
 
 --
--- Name: RefreshToken RefreshToken_UserId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: Friendships Friendship_UserId2_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."RefreshToken"
-    ADD CONSTRAINT "RefreshToken_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES public."User"("Id");
+ALTER TABLE ONLY public."Friendships"
+    ADD CONSTRAINT "Friendship_UserId2_fkey" FOREIGN KEY ("UserId2") REFERENCES public."Users"("Id");
+
+
+--
+-- Name: RefreshTokens RefreshToken_UserId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."RefreshTokens"
+    ADD CONSTRAINT "RefreshToken_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES public."Users"("Id");
 
 
 --
@@ -334,7 +334,7 @@ ALTER TABLE ONLY public."RefreshToken"
 --
 
 ALTER TABLE ONLY public."RoleRights"
-    ADD CONSTRAINT "RoleRights_RightId_fkey" FOREIGN KEY ("RightId") REFERENCES public."Right"("Id");
+    ADD CONSTRAINT "RoleRights_RightId_fkey" FOREIGN KEY ("RightId") REFERENCES public."Rights"("Id");
 
 
 --
@@ -342,47 +342,47 @@ ALTER TABLE ONLY public."RoleRights"
 --
 
 ALTER TABLE ONLY public."RoleRights"
-    ADD CONSTRAINT "RoleRights_RoleId_fkey" FOREIGN KEY ("RoleId") REFERENCES public."Role"("Id");
+    ADD CONSTRAINT "RoleRights_RoleId_fkey" FOREIGN KEY ("RoleId") REFERENCES public."Roles"("Id");
 
 
 --
--- Name: User User_RoleId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: Users User_RoleId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."User"
-    ADD CONSTRAINT "User_RoleId_fkey" FOREIGN KEY ("RoleId") REFERENCES public."Role"("Id");
-
-
---
--- Name: FriendRequest friendrequest_ReceiverUserId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."FriendRequest"
-    ADD CONSTRAINT "friendrequest_ReceiverUserId_fkey" FOREIGN KEY ("ReceiverUserId") REFERENCES public."User"("Id");
+ALTER TABLE ONLY public."Users"
+    ADD CONSTRAINT "User_RoleId_fkey" FOREIGN KEY ("RoleId") REFERENCES public."Roles"("Id");
 
 
 --
--- Name: FriendRequest friendrequest_SenderUserId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: FriendRequests friendrequest_ReceiverUserId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."FriendRequest"
-    ADD CONSTRAINT "friendrequest_SenderUserId_fkey" FOREIGN KEY ("SenderUserId") REFERENCES public."User"("Id");
-
-
---
--- Name: Session session_UserId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."Session"
-    ADD CONSTRAINT "session_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES public."User"("Id");
+ALTER TABLE ONLY public."FriendRequests"
+    ADD CONSTRAINT "friendrequest_ReceiverUserId_fkey" FOREIGN KEY ("ReceiverUserId") REFERENCES public."Users"("Id");
 
 
 --
--- Name: UserProfile userprofiles_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: FriendRequests friendrequest_SenderUserId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."UserProfile"
-    ADD CONSTRAINT userprofiles_user_id_fkey FOREIGN KEY ("UserId") REFERENCES public."User"("Id");
+ALTER TABLE ONLY public."FriendRequests"
+    ADD CONSTRAINT "friendrequest_SenderUserId_fkey" FOREIGN KEY ("SenderUserId") REFERENCES public."Users"("Id");
+
+
+--
+-- Name: Sessions session_UserId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Sessions"
+    ADD CONSTRAINT "session_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES public."Users"("Id");
+
+
+--
+-- Name: UserProfiles userprofiles_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."UserProfiles"
+    ADD CONSTRAINT userprofiles_user_id_fkey FOREIGN KEY ("UserId") REFERENCES public."Users"("Id");
 
 
 --
