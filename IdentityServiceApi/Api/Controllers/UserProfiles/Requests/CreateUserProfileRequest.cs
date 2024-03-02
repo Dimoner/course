295f0a4d6fdc602e657778f2e136c70871cce8f2
daHil <ssocmain@gmail.com>
2024-03-02 14:04:53 +0500

    added user profile requests models

---
+++ IdentityServiceApi/Api/Controllers/UserProfiles/Requests/CreateUserProfileRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Controllers.UserProfiles.Requests
+{
+    public class CreateUserProfileRequest
+    {
+        [Required]
+        public Guid UserId { get; init; }
+        
+        [Required]
+        public string? Status { get; init; }
+
+        [Required]
+        public DateTime? BirthDate { get; init; }
+
+        [Required]
+        [Url]
+        public string AvatarUrl { get; init; } = null!;
+    }
+}
