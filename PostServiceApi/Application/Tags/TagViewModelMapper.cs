c327cb819e9529d47fd1e0d1da7116cc3b8a47fe
daHil <ssocmain@gmail.com>
2024-03-03 15:37:52 +0500

    added tag view model mapper

---
+++ PostServiceApi/Application/Tags/TagViewModelMapper.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using Domain.Tags;
+
+namespace Application.Tags
+{
+    public class TagViewModelMapper : IMapper<TagViewModel, Tag>
+    {
+        public TagViewModel Map(Tag entity)
+        {
+            return new TagViewModel
+            {
+                Id = entity.Id,
+                Value = entity.Value,
+            };
+        }
+
+        public IEnumerable<TagViewModel> Map(IEnumerable<Tag> entities)
+        {
+            return entities.Select(Map);
+        }
+    }
+}
