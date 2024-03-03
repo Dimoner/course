f32367fda847e49cf792d9fefece7070cdc29d50
daHil <ssocmain@gmail.com>
2024-03-03 15:39:15 +0500

    added post like view model

---
+++ PostServiceApi/Application/PostLikes/PostLikeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.PostLikes
+{
+    public record PostLikeViewModel
+    {
+        /// <summary>
+        /// Id of the PostLike
+        /// </summary>
+        [Required]
+        public Guid Id { get; init; }
+
+        /// <summary>
+        /// Id of the post that this like was put on
+        /// </summary>
+        [Required]
+        public Guid PostId { get; init; }
+
+        /// <summary>
+        /// Id of the user that put this like
+        /// </summary>
+        [Required]
+        public Guid UserId { get; init; }
+    }
+}
