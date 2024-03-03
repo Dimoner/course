58d33a3bd7990a2d336bcc649fcd03fb8ca3eca4
daHil <ssocmain@gmail.com>
2024-03-03 23:49:40 +0500

    added interface for getting view model

---
+++ PostServiceApi/Application/Interfaces/Services/IGetPageService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Interfaces.Services
+{
+    public interface IGetPageService<TViewModel> where TViewModel : class
+    {
+        Task<IEnumerable<TViewModel>> GetPageAsync(int pageNumber, int pageSize);
+    }
+}
