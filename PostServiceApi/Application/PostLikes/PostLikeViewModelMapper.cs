eabdb97c778a354d638253637a988af171808f26
daHil <ssocmain@gmail.com>
2024-03-03 15:39:35 +0500

    added post like view model mapper

---
+++ PostServiceApi/Application/PostLikes/PostLikeViewModelMapper.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Domain.PostLikes;
+
+namespace Application.PostLikes
+{
+    public class PostLikeViewModelMapper : IMapper<PostLikeViewModel, PostLike>
+    {
+        public PostLikeViewModel Map(PostLike entity)
+        {
+            return new PostLikeViewModel
+            {
+                Id = entity.Id,
+                PostId = entity.PostId,
+                UserId = entity.UserId,
+            };
+        }
+
+        public IEnumerable<PostLikeViewModel> Map(IEnumerable<PostLike> entities)
+        {
+            return entities.Select(Map);
+        }
+    }
+}
