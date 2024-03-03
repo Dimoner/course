55abdf04112919e65fe3e13d6e1d70dc2574413e
daHil <ssocmain@gmail.com>
2024-03-03 15:36:54 +0500

    added base mapper interface

---
+++ PostServiceApi/Application/Interfaces/IMapper.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interfaces
+{
+    public interface IMapper<TViewModel, TEntity>
+    {
+        public TViewModel Map(TEntity entity);
+        public IEnumerable<TViewModel> Map(IEnumerable<TEntity> entities);
+    }
+}
