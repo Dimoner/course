8c188a77652dd6d95f9a2c1182eed3e9f256bedb
daHil <ssocmain@gmail.com>
2024-03-03 15:20:35 +0500

    added tag entity

---
+++ PostServiceApi/Domain/Tags/Tag.cs
@@ -0,0 +1,12 @@
+﻿using Core.Dal.Base;
+
+namespace Domain.Tags
+{
+    public record Tag : BaseEntity
+    {
+        /// <summary>
+        /// The tag's value
+        /// </summary>
+        public string Value { get; init; } = null!;
+    }
+}
