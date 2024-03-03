9191b64ee8b4f91b2695b3168a184651c0819b2c
daHil <ssocmain@gmail.com>
2024-03-03 23:47:08 +0500

    added exception for the case when no comment is found

---
+++ PostServiceApi/Domain/Comments/Exceptions/CommentNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using Core.Logic.Base.Exceptions;
+
+namespace Domain.Comments.Exceptions
+{
+    public class CommentNotFoundException : NotFoundException
+    {
+        public CommentNotFoundException(Guid commentId) :
+            base($"The comment with the identifier {commentId} was not found.")
+        {
+        }
+    }
+}
