72dc21ec18317272e71d8633e62ae7c8fa82c1b6
daHil <ssocmain@gmail.com>
2024-03-03 15:25:04 +0500

    added comment repo interface

---
+++ PostServiceApi/Domain/Comments/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Dal.Base;
+using Domain.Interfaces;
+
+namespace Domain.Comments
+{
+    public interface ICommentRepository : IRepository<Comment>
+    {
+        /// <summary>
+        /// Get list of Comments that refer to 
+        /// a certain Post which is decided by Post id
+        /// </summary>
+        Task<PageList<Comment>> GetCommentsPageByPostIdAsync(Guid postId);
+    }
+}
