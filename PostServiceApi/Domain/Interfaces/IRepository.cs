f2825e021d292be6c8df71da83cbe56796ad700e
daHil <ssocmain@gmail.com>
2024-03-03 15:18:44 +0500

    added base Repository interface

---
+++ PostServiceApi/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,37 @@
+﻿using Core.Dal.Base;
+
+namespace Domain.Interfaces
+{
+    /// <summary>
+    /// Base repository interface for every other repositories 
+    /// that would be designed for a specific entity
+    /// </summary>
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Gettng entity by id 
+        /// </summary>
+        Task<TEntity?> GetAsync(Guid id);
+
+        /// <summary>
+        /// Get list of entities of a certain size 
+        /// starting from a certain point in the db
+        /// </summary>
+        Task<PageList<TEntity>> GetPageAsync(int pageNumber, int pageSize);
+
+        /// <summary>
+        /// Add entity to db
+        /// </summary>
+        Task<Guid> CreateAsync(TEntity entity);
+
+        /// <summary>
+        /// Update or create entity
+        /// </summary>
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        /// <summary>
+        /// Delete entity by id
+        /// </summary>
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
