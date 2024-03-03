6563fd1e1cdb2b111dca4dc4b8526bfeee6c94be
daHil <ssocmain@gmail.com>
2024-03-03 15:24:13 +0500

    added post like repo interface

---
+++ PostServiceApi/Domain/PostLikes/IPostLikeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Dal.Base;
+using Domain.Interfaces;
+
+namespace Domain.PostLikes
+{
+    public interface IPostLikeRepository : IRepository<PostLike>
+    {
+        /// <summary>
+        /// Get list of PostLikes that refer to 
+        /// a certain Post which is decided by Post id
+        /// </summary>
+        Task<PageList<PostLike>> GetPostLikesPageByPostIdAsync(Guid postId);
+    }
+}
