619caada8ec54b05d23a686c444b4c404b22e41c
daHil <ssocmain@gmail.com>
2024-03-03 15:37:29 +0500

    added tag view model

---
+++ PostServiceApi/Application/Tags/TagViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Tags
+{
+    public record TagViewModel
+    {
+        /// <summary>
+        /// Id of the tag
+        /// </summary>
+        [Required]
+        public Guid Id { get; init; }
+
+        /// <summary>
+        /// The tag's value
+        /// </summary>
+        [Required]
+        [MinLength(2)]
+        [MaxLength(30)]
+        public string Value { get; init; } = null!;
+    }
+}
