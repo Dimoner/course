719d036de52002ea4ca2bac73e9bf4e328b8c5a7
daHil <ssocmain@gmail.com>
2024-03-03 15:24:37 +0500

    added comment entity

---
+++ PostServiceApi/Domain/Comments/Comment.cs
@@ -0,0 +1,32 @@
+﻿using Core.Dal.Base;
+
+namespace Domain.Comments
+{
+    public record Comment : BaseEntity
+    {
+        /// <summary>
+        /// Id of the user that created this comment
+        /// </summary>
+        public Guid UserId { get; init; }
+
+        /// <summary>
+        /// Id of the post to which this comment refers
+        /// </summary>
+        public Guid PostId { get; init; }
+
+        /// <summary>
+        /// Content of this comment
+        /// </summary>
+        public string Content { get; init; } = null!;
+
+        /// <summary>
+        /// The date and time of the comment creation
+        /// </summary>
+        public DateTime CreatedAt { get; init; }
+
+        /// <summary>
+        /// The date and time of the comment last update
+        /// </summary>
+        public DateTime UpdatedAt { get; init; }
+    }
+}
