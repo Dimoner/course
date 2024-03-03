8f19d0e2ca64c90d8ab87989bba86dc249a5dcbe
daHil <ssocmain@gmail.com>
2024-03-03 15:40:27 +0500

    added comment view model mapper

---
+++ PostServiceApi/Application/Comments/CommentViewModelMapper.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Domain.Comments;
+
+namespace Application.Comments
+{
+    public class CommentViewModelMapper : IMapper<CommentViewModel, Comment>
+    {
+        public CommentViewModel Map(Comment entity)
+        {
+            return new CommentViewModel
+            {
+                Id = entity.Id,
+                UserId = entity.UserId,
+                PostId = entity.PostId,
+                Content = entity.Content,
+                CreatedAt = entity.CreatedAt,
+                UpdatedAt = entity.UpdatedAt
+            };
+        }
+
+        public IEnumerable<CommentViewModel> Map(IEnumerable<Comment> entities)
+        {
+            return entities.Select(Map);
+        }
+    }
+}
