78e573b7ab28e937de9946cec1aee7d216619a8d
daHil <ssocmain@gmail.com>
2024-03-03 23:50:52 +0500

    added comment service

---
+++ PostServiceApi/Application/Comments/Services/CommentService.cs
@@ -0,0 +1,62 @@
+﻿
+using Application.Comments.Mappers;
+using Domain.Comments;
+using Domain.Comments.Exceptions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Comments.Services
+{
+    internal sealed class CommentService : ICommentService
+    {
+        private readonly ICommentRepository commentRepository;
+        private readonly ICommentViewModelMapper commentViewModelMapper;
+
+        public CommentService(ICommentRepository commentRepository)
+        {
+            this.commentRepository = commentRepository;
+        }
+
+        public async Task<Guid> CreateAsync(CommentViewModel viewModel)
+        {
+            var entity =  commentViewModelMapper.Map(viewModel);
+
+            return await commentRepository.CreateAsync(entity);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await commentRepository.DeleteAsync(id);
+        }
+
+        public async Task<CommentViewModel> GetAsync(Guid id)
+        {
+            var entity =  await commentRepository.GetAsync(id) ?? throw new CommentNotFoundException(id);
+            var viewModel = commentViewModelMapper.Map(entity);
+
+            return viewModel;
+        }
+
+        public async Task<IEnumerable<CommentViewModel>> GetPageAsync(int pageNumber, int pageSize)
+        {
+            var page = await commentRepository.GetPageAsync(pageNumber, pageSize);
+
+            return page.Select(commentViewModelMapper.Map);
+        }
+
+        public async Task<IEnumerable<CommentViewModel>> GetPostComments(Guid postId)
+        {
+            var comments = await commentRepository.GetCommentsByPostIdAsync(postId);
+
+            return comments.Select(commentViewModelMapper.Map);
+        }
+
+        public async Task UpdateAsync(CommentViewModel viewModel)
+        {
+            if (await commentRepository.GetAsync(viewModel.Id) is null)
+                throw new CommentNotFoundException(viewModel.Id);
+
+            var entity = commentViewModelMapper.Map(viewModel);
+            await commentRepository.UpdateAsync(entity);
+        }
+    }
+}
---
+++ PostServiceApi/Application/Comments/Services/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interfaces.Services;
+
+namespace Application.Comments.Services
+{
+    public interface ICommentService : 
+        ICreateService<CommentViewModel>,
+        IUpdateService<CommentViewModel>,
+        IDeleteService<CommentViewModel>,
+        IGetPageService<CommentViewModel>,
+        IGetService<CommentViewModel>
+    {
+        Task<IEnumerable<CommentViewModel>> GetPostComments(Guid postId);
+    }
+}
