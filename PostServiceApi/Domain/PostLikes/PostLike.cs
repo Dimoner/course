6e327acd40aff38da48981e86d7094c302df6b48
daHil <ssocmain@gmail.com>
2024-03-03 15:23:48 +0500

    added post like entity

---
+++ PostServiceApi/Domain/PostLikes/PostLike.cs
@@ -0,0 +1,17 @@
+﻿using Core.Dal.Base;
+
+namespace Domain.PostLikes
+{
+    public record PostLike : BaseEntity
+    {
+        /// <summary>
+        /// Id of the post that this like was put on
+        /// </summary>
+        public Guid PostId { get; init; }
+
+        /// <summary>
+        /// Id of the user that put this like
+        /// </summary>
+        public Guid UserId { get; init; }
+    }
+}   
