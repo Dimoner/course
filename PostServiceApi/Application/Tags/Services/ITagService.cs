7d18938d29f7273ad3298b7dacef5b7ac0a76bb7
daHil <ssocmain@gmail.com>
2024-03-04 14:12:34 +0500

    added interface of tag service

---
+++ PostServiceApi/Application/Tags/Services/ITagService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Interfaces.Services;
+
+namespace Application.Tags.Services
+{
+    public interface ITagService :
+        ICreateService<TagViewModel>,
+        IDeleteService<TagViewModel>,
+        IGetPageService<TagViewModel>,
+        IGetService<TagViewModel>
+    {
+    }
+}
