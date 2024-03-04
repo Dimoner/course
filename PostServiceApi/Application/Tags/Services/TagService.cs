f511d0dedb3bd7e5e3aa9f8311a4768e724f14f1
daHil <ssocmain@gmail.com>
2024-03-04 14:13:02 +0500

    added tag service interface implementation

---
+++ PostServiceApi/Application/Tags/Services/TagService.cs
@@ -0,0 +1,45 @@
+﻿using Application.Tags.Mappers;
+using Domain.Tags;
+using Domain.Tags.Exceptions;
+
+namespace Application.Tags.Services
+{
+    internal sealed class TagService : ITagService
+    {
+        private readonly ITagRepository tagRepository;
+        private readonly ITagViewModelMapper tagViewModelMapper;
+
+        public TagService(ITagRepository tagRepository, ITagViewModelMapper tagViewModelMapper)
+        {
+            this.tagRepository = tagRepository;
+            this.tagViewModelMapper = tagViewModelMapper;
+        }
+
+        public async Task<Guid> CreateAsync(TagViewModel viewModel)
+        {
+            var entity = tagViewModelMapper.Map(viewModel);
+
+            return await tagRepository.CreateAsync(entity);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await tagRepository.DeleteAsync(id);
+        }
+
+        public async Task<TagViewModel> GetAsync(Guid id)
+        {
+            var entity = await tagRepository.GetAsync(id) ?? throw new TagNotFoundException(id);
+            var viewModel = tagViewModelMapper.Map(entity);
+
+            return viewModel;
+        }
+
+        public async Task<IEnumerable<TagViewModel>> GetPageAsync(int pageNumber, int pageSize)
+        {
+            var page = await tagRepository.GetPageAsync(pageNumber, pageSize);
+
+            return page.Select(tagViewModelMapper.Map);
+        }
+    }
+}
