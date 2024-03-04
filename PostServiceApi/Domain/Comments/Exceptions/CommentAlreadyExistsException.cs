bf4d677b20c16b6106c16feafbc1c7fea271fad1
daHil <ssocmain@gmail.com>
2024-03-04 14:50:34 +0500

    added exception for the case when trying to add a comment that already exists

---
+++ PostServiceApi/Domain/Comments/Exceptions/CommentAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿using Core.Logic.Base.Exceptions;
+using System.ComponentModel.Design;
+
+namespace Domain.Comments.Exceptions
+{
+    public class CommentAlreadyExistsException : BadRequestException
+    {
+        public CommentAlreadyExistsException(Guid commentId) : base($"The comment with the identifier {commentId} already exists.")
+        {
+        }
+    }
+}
