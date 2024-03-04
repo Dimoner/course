95e17233447e145f5b19eb5827b9a6c4572789c3
daHil <ssocmain@gmail.com>
2024-03-04 20:20:09 +0500

    added controllers options

--- PostServiceApi/Api/Program.cs
+++ PostServiceApi/Api/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+    options.ReturnHttpNotAcceptable = true;
+    options.RespectBrowserAcceptHeader = true;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
