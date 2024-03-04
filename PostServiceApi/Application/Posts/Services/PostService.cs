e97daf8487369f30aec3ca39cfd5f14fe4b3ab65
daHil <ssocmain@gmail.com>
2024-03-04 14:13:44 +0500

    added implementation of post service interface

---
+++ PostServiceApi/Application/Posts/Services/PostService.cs
@@ -0,0 +1,54 @@
+﻿using Application.Posts.Mappers;
+using Domain.Posts;
+using Domain.Posts.Exceptions;
+
+namespace Application.Posts.Services
+{
+    internal sealed class PostService : IPostService
+    {
+        private readonly IPostRepository postRepository;
+        private readonly IPostViewModelMapper postMapper;
+
+        public PostService(IPostRepository postRepository, IPostViewModelMapper postMapper)
+        {
+            this.postRepository = postRepository;
+            this.postMapper = postMapper;
+        }
+
+        public async Task<Guid> CreateAsync(PostViewModel viewModel)
+        {
+            var entity = postMapper.Map(viewModel);
+
+            return await postRepository.CreateAsync(entity);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await postRepository.DeleteAsync(id);
+        }
+
+        public async Task<PostViewModel> GetAsync(Guid id)
+        {
+            var entity = await postRepository.GetAsync(id) ?? throw new PostNotFoundException(id);
+            var viewModel = postMapper.Map(entity);
+
+            return viewModel;
+        }
+
+        public async Task<IEnumerable<PostViewModel>> GetPageAsync(int pageNumber, int pageSize)
+        {
+            var page = await postRepository.GetPageAsync(pageNumber, pageSize);
+
+            return page.Select(postMapper.Map);
+        }
+
+        public async Task UpdateAsync(PostViewModel viewModel)
+        {
+            if(await postRepository.GetAsync(viewModel.Id) is null)
+                throw new PostNotFoundException(viewModel.Id);
+
+            var entity = postMapper.Map(viewModel);
+            await postRepository.UpdateAsync(entity);
+        }
+    }
+}
