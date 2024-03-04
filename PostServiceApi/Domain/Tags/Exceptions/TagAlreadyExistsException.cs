d6e269341b65730d01cd9093fe282b651aceb647
daHil <ssocmain@gmail.com>
2024-03-04 15:35:53 +0500

    added exception for the case when trying to add tag that already exists

---
+++ PostServiceApi/Domain/Tags/Exceptions/TagAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿using Core.Logic.Base.Exceptions;
+
+namespace Domain.Tags.Exceptions
+{
+    public class TagAlreadyExistsException : BadRequestException
+    {
+        public TagAlreadyExistsException(Guid tagId) : base($"The tag with the identifier {tagId} already exists.")
+        {
+        }
+    }
+}
