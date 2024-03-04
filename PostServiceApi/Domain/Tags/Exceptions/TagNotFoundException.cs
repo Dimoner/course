859ab0c50024c671f24f79639127f88cba34966e
daHil <ssocmain@gmail.com>
2024-03-04 14:11:41 +0500

    added exception for the case when tag is not found

---
+++ PostServiceApi/Domain/Tags/Exceptions/TagNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Core.Logic.Base.Exceptions;
+
+namespace Domain.Tags.Exceptions
+{
+    public class TagNotFoundException : NotFoundException
+    {
+        public TagNotFoundException(Guid tagId) : base($"The tag with the identifier {tagId} was not found.")
+        {
+        }
+    }
+}
