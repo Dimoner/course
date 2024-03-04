2abb338d806ab508bf3455666e1498f7d21fe3b9
daHil <ssocmain@gmail.com>
2024-03-04 15:23:32 +0500

    added exception for the case when trying to add a postLike that already exists

---
+++ PostServiceApi/Domain/PostLikes/Exceptions/PostLikeAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿using Core.Logic.Base.Exceptions;
+
+namespace Domain.PostLikes.Exceptions
+{
+    public class PostLikeAlreadyExistsException : BadRequestException
+    {
+        public PostLikeAlreadyExistsException(Guid postLikeId) : base($"The post like with the identifier {postLikeId} already exists.")
+        {
+        }
+    }
+}
