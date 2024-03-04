6b3c72e162956aeac863cc79a822d80fbb91bd8b
daHil <ssocmain@gmail.com>
2024-03-04 14:12:05 +0500

    added exception for the case when post is not found

---
+++ PostServiceApi/Domain/Posts/Exceptions/PostNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Core.Logic.Base.Exceptions;
+
+namespace Domain.Posts.Exceptions
+{
+    public class PostNotFoundException : NotFoundException
+    {
+        public PostNotFoundException(Guid postId) : base($"The post with the identifier {postId} was not found.")
+        {
+        }
+    }
+}
