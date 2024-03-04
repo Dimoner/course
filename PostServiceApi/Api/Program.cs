e098ed77b4c92396544f5e67cc648ebe7363d1b7
daHil <ssocmain@gmail.com>
2024-03-04 22:31:57 +0500

    now program configures service using startup

--- PostServiceApi/Api/Program.cs
+++ PostServiceApi/Api/Program.cs
@@ -1,63 +1,17 @@
-using Application.Comments.Services;
-using Application.PostLikes.Services;
-using Application.Posts.Services;
-using Application.Tags.Services;
-using Domain.Comments;
-using Domain.PostLikes;
-using Domain.Posts;
-using Domain.Tags;
-using Infrastructure.Comments;
-using Infrastructure.PostLikes;
-using Infrastructure.Posts;
-using Infrastructure.Tags;
-using Microsoft.AspNetCore.Mvc.Formatters;
-
-internal static class Program
+namespace Api
 {
-    private static void Main(string[] args)
+    internal static class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers(options =>
+        private static void Main(string[] args)
         {
-            options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
-            options.ReturnHttpNotAcceptable = true;
-            options.RespectBrowserAcceptHeader = true;
-        }).AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
-
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        // Adding repositories
-        builder.Services.AddScoped<ICommentRepository, CommentsRepository>();
-        builder.Services.AddScoped<IPostLikeRepository, PostLikeRepository>();
-        builder.Services.AddScoped<IPostRepository, PostRepository>();
-        builder.Services.AddScoped<ITagRepository, TagRepository>();
-
-        // Adding services
-        builder.Services.AddScoped<ICommentService, CommentService>();
-        builder.Services.AddScoped<IPostLikeService, PostLikeService>();
-        builder.Services.AddScoped<IPostService, PostService>();
-        builder.Services.AddScoped<ITagService, TagService>();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
     }
 }
