2356569069d3ecb635b6e576624acf11495eee91
daHil <ssocmain@gmail.com>
2024-03-04 21:47:45 +0500

    refactored

--- PostServiceApi/Presentation/AssemblyReference.cs
+++ PostServiceApi/Presentation/AssemblyReference.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Presentation
+﻿namespace Presentation
 {
-    internal class AssemblyReference
+    public static class AssemblyReference
     {
     }
 }
