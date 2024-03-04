debaa09cf6508f3fac69bd7fc036594283645d20
daHil <ssocmain@gmail.com>
2024-03-04 15:29:35 +0500

    added exception for the case when trying to add a post that already exists

---
+++ PostServiceApi/Domain/Posts/Exceptions/PostAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿using Core.Logic.Base.Exceptions;
+
+namespace Domain.Posts.Exceptions
+{
+    public class PostAlreadyExistsException : BadRequestException
+    {
+        public PostAlreadyExistsException(Guid postId) : base($"The post with the identifier {postId} already exists.")
+        {
+        }
+    }
+}
