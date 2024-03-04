77618b5c92398637a58d05f521e1f09605a7a521
daHil <ssocmain@gmail.com>
2024-03-04 19:13:22 +0500

    made post id an index

--- PostServiceApi/Infrastructure/Comments/CommentConfiguration.cs
+++ PostServiceApi/Infrastructure/Comments/CommentConfiguration.cs
@@ -9,12 +9,16 @@ namespace Infrastructure.Comments
         public void Configure(EntityTypeBuilder<Comment> builder)
         {
             builder.ToTable(nameof(Comment));
+
             builder.HasKey(comment => comment.Id);
+
             builder.Property(comment => comment.UserId).IsRequired();
             builder.Property(comment => comment.PostId).IsRequired();
             builder.Property(comment => comment.Content).IsRequired();
             builder.Property(comment => comment.CreatedAt).IsRequired();
             builder.Property(comment => comment.UpdatedAt);
+
+            builder.HasIndex(comment => comment.PostId);
         }
     }
 }
