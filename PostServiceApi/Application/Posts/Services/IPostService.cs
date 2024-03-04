dc9eeba80c71feda70857ed8b7b37ab628d51033
daHil <ssocmain@gmail.com>
2024-03-04 14:13:27 +0500

    added interface of post service

---
+++ PostServiceApi/Application/Posts/Services/IPostService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces.Services;
+
+namespace Application.Posts.Services
+{
+    public interface IPostService :
+        ICreateService<PostViewModel>,
+        IUpdateService<PostViewModel>,
+        IDeleteService<PostViewModel>,
+        IGetPageService<PostViewModel>,
+        IGetService<PostViewModel>
+    {
+    }
+}
