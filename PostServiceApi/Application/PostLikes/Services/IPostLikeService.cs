9d4cb42d597b13ecc7aab0d083817d419199b833
daHil <ssocmain@gmail.com>
2024-03-04 13:47:58 +0500

    added interface of post like service

---
+++ PostServiceApi/Application/PostLikes/Services/IPostLikeService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Comments;
+using Application.Interfaces.Services;
+using Domain.PostLikes;
+
+namespace Application.PostLikes.Services
+{
+    public interface IPostLikeService :
+        ICreateService<PostLikeViewModel>,
+        IDeleteService<PostLikeViewModel>,
+        IGetPageService<PostLikeViewModel>,
+        IGetService<PostLikeViewModel>
+    {
+        Task<IEnumerable<PostLikeViewModel>> GetPostLikesPageByPostIdAsync(Guid postId, int pageNumber, int pageSize);
+    }
+}
