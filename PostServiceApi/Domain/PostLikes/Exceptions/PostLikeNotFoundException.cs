c630cb7d9fbf7689668b270fe89e26a2b074a967
daHil <ssocmain@gmail.com>
2024-03-04 13:46:31 +0500

    added exception for the case when post like is not found

---
+++ PostServiceApi/Domain/PostLikes/Exceptions/PostLikeNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Core.Logic.Base.Exceptions;
+
+namespace Domain.PostLikes.Exceptions
+{
+    public class PostLikeNotFoundException : NotFoundException
+    {
+        public PostLikeNotFoundException(Guid postLikeId) : base($"The post like with the identifier {postLikeId} was not found.")
+        {
+        }
+    }
+}
